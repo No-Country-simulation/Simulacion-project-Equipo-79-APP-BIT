495b0288e37a4fbf6b5bc4a376264ff0fc0360be
calebsenm <calebsenm@gmail.com>
2026-07-03 15:29:38 -0500

    fix: router

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import RootLayout from './RootLayout.jsx';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <RootLayout />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
